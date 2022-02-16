f624d93704422604b46e0034f3424ec929743b0b
unknown <mariana.avilesmartinez@gmail.com.com>
2022-02-16 10:57:15 -0600

    Proyecto Spring Inicializado

---
+++ src/main/java/com/example/MaterialesConstruccion/MaterialesConstruccionApplication.java
@@ -0,0 +1,13 @@
+package com.example.MaterialesConstruccion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaterialesConstruccionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MaterialesConstruccionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/MaterialesConstruccion/MaterialesConstruccionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.MaterialesConstruccion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaterialesConstruccionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
